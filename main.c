76a773df48c794a613a59bc342e0d6b2b8d5f4f3
Lohan Lecoq <lohan.lecoq@epitech.eu>
2025-04-01 10:11:04 +0200

    refactor(main): removed cs from the main

--- main.c
+++ main.c
@@ -1,9 +1,11 @@
+/*
+** EPITECH PROJECT, 2025
+** B-DOP-200-MPL-2-1-chocolatine-lohan.lecoq
+** File description:
+** main
+*/
 
 int main(void)
-{
-    if(1==2)
-        return 1;
-    // COUCOU
-    printf("Hello World\n");    
+{   
     return 0;
 }
